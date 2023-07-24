2147aa6f087345e8ecaee49ea1d9eca942475f69
17146 <1714673995@qq.com>
2023-07-24 21:11:29 +0800

    注册了冰淇淋红茶

--- src/main/java/top/yora/virtuarealcraft/init/ItemRegistry.java
+++ src/main/java/top/yora/virtuarealcraft/init/ItemRegistry.java
@@ -6,9 +6,16 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import top.yora.virtuarealcraft.Utils;
 import top.yora.virtuarealcraft.item.others.VrcLogo;
+import top.yora.virtuarealcraft.item.virtuareal2nd.nanami.IceCreamBlackTea;
 
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
 
     public static final RegistryObject<Item> VRC_LOGO = ITEMS.register("vrc_logo", VrcLogo::new);
+
+    /**
+     * 2nd
+     */
+    //nanami
+    public static final RegistryObject<Item> ICE_CREAM_BLACK_TEA = ITEMS.register("ice_cream_black_tea", IceCreamBlackTea::new);
 }
---
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal2nd/nanami/IceCreamBlackTea.java
@@ -0,0 +1,10 @@
+package top.yora.virtuarealcraft.item.virtuareal2nd.nanami;
+
+import net.minecraft.item.Item;
+import top.yora.virtuarealcraft.group.ModGroup;
+
+public class IceCreamBlackTea extends Item {
+    public IceCreamBlackTea() {
+        super(new Properties().group(ModGroup.itemgroup));
+    }
+}
