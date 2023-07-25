4a2fb135b59e72b0535239f14ff9dda8082c38d8
17146 <1714673995@qq.com>
2023-07-25 15:17:51 +0800

    添加了仓鼠飞轮的模型和材质

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal5th/hoshimi/HamsterWheel.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal5th/hoshimi/HamsterWheel.java
@@ -1,15 +1,22 @@
 package top.yora.virtuarealcraft.item.virtuareal5th.hoshimi;
 
+import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import top.yora.virtuarealcraft.Utils;
 import top.yora.virtuarealcraft.group.ModGroup;
+import top.yora.virtuarealcraft.models.HamsterWheelModel;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
 
@@ -25,9 +32,21 @@ public class HamsterWheel extends ArmorItem {
     @OnlyIn(Dist.CLIENT)
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-
+        tooltip.add(new TranslationTextComponent("des.virtuarealcraft.hamster_wheel").mergeStyle(TextFormatting.GRAY));
 
         TooltipTool.addLiverInfo(tooltip, Livers.HOSHIMI);
     }
 
+    @SuppressWarnings("unchecked")
+    @Nullable
+    @Override
+    public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
+        return (A) new HamsterWheelModel<>();
+    }
+
+    @Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return Utils.MOD_ID + ":textures/models/armor/hamster_wheel.png";
+    }
 }
