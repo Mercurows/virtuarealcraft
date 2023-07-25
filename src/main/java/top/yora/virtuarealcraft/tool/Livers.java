1be07c6a2f1bcfc0cb72ea060135dfd7d886362b
17146 <1714673995@qq.com>
2023-07-25 14:35:07 +0800

    造轮子

--- src/main/java/top/yora/virtuarealcraft/tool/Livers.java
+++ src/main/java/top/yora/virtuarealcraft/tool/Livers.java
@@ -2,8 +2,19 @@ package top.yora.virtuarealcraft.tool;
 
 public enum Livers {
     NANAMI("nanami", "2nd", "#418BDE", true),
+    KOUICHI("kouichi", "2nd", "#E6E1D2", true),
+    YUKIE("yukie","7th","#FFECC7",true),
     CHIHARU("chiharu", "9th", "#8C6250", true),
-    SUI("sui","17th","#87EAFF", true);
+    SHAUN("shaun","10th","#DFD6D3",true),
+    MARI("mari","11th","#C70039", true),
+    CHIYUU("chiyuu","11th","#CC225F",true),
+    IMI("imi","11th","#CC777C",true),
+    MAYUMI("mayumi","12th","#D6E568",true),
+    KOXIA("koxia","14th","#EE548E",false),
+    GIRIMI("girimi","16th","#1E377F",true),
+    KIYUU("kiyuu","16th","#FF91AE",true),
+    SUI("sui","17th","#87EAFF", true),
+    AMEKI("ameki","19th","#9EEEFF",true);
 
     private final String name;
     private final String gen;
