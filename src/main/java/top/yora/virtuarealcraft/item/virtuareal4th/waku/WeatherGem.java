14f373fea30d79645bf4ff48572a89c54c8bbfee
17146 <1714673995@qq.com>
2023-07-29 00:03:35 +0800

    添加了天气宝石的动画效果

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal4th/waku/WeatherGem.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal4th/waku/WeatherGem.java
@@ -1,5 +1,6 @@
 package top.yora.virtuarealcraft.item.virtuareal4th.waku;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -16,6 +17,7 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import top.yora.virtuarealcraft.group.ModGroup;
+import top.yora.virtuarealcraft.init.ItemRegistry;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
 
@@ -38,38 +40,58 @@ public class WeatherGem extends Item {
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        boolean flag = false;
+
         ItemStack itemStack = playerIn.getHeldItem(handIn);
-        if(handIn == Hand.MAIN_HAND && !worldIn.isRemote){
-            ItemStack off = playerIn.getHeldItemOffhand();
-            if(off.isEmpty()){
-                return new ActionResult<>(ActionResultType.FAIL, itemStack);
-            }else {
-                if(off.getItem() == Items.TORCH){
-                    off.shrink(1);
+        if(handIn == Hand.MAIN_HAND){
+            if(!worldIn.isRemote) {
+                ItemStack off = playerIn.getHeldItemOffhand();
+                if (off.isEmpty()) {
+                    return new ActionResult<>(ActionResultType.FAIL, itemStack);
+                } else {
+                    if (off.getItem() == Items.TORCH) {
+                        off.shrink(1);
 
-                    ((ServerWorld) worldIn).func_241113_a_(12000, 0, false, false);
-                    itemStack.damageItem(1, playerIn, p -> p.sendBreakAnimation(handIn));
-                    playerIn.getCooldownTracker().setCooldown(itemStack.getItem(), 6000);
+                        ((ServerWorld) worldIn).func_241113_a_(12000, 0, false, false);
+                        itemStack.damageItem(1, playerIn, p -> p.sendBreakAnimation(handIn));
+                        playerIn.getCooldownTracker().setCooldown(itemStack.getItem(), 6000);
 
-                    return new ActionResult<>(ActionResultType.SUCCESS, itemStack);
-                }else if(off.getItem() == Items.WHEAT_SEEDS){
-                    off.shrink(1);
+                        flag = true;
+                    } else if (off.getItem() == Items.WHEAT_SEEDS) {
+                        off.shrink(1);
 
-                    ((ServerWorld) worldIn).func_241113_a_(0, 12000, true, false);
-                    itemStack.damageItem(1, playerIn, p -> p.sendBreakAnimation(handIn));
-                    playerIn.getCooldownTracker().setCooldown(itemStack.getItem(), 6000);
+                        ((ServerWorld) worldIn).func_241113_a_(0, 12000, true, false);
+                        itemStack.damageItem(1, playerIn, p -> p.sendBreakAnimation(handIn));
+                        playerIn.getCooldownTracker().setCooldown(itemStack.getItem(), 6000);
 
-                    return new ActionResult<>(ActionResultType.SUCCESS, itemStack);
-                }else if(off.getItem() == Items.GUNPOWDER){
-                    off.shrink(1);
+                        flag = true;
+                    } else if (off.getItem() == Items.GUNPOWDER) {
+                        off.shrink(1);
 
-                    ((ServerWorld) worldIn).func_241113_a_(0, 12000, true, true);
-                    itemStack.damageItem(1, playerIn, p -> p.sendBreakAnimation(handIn));
-                    playerIn.getCooldownTracker().setCooldown(itemStack.getItem(), 6000);
+                        ((ServerWorld) worldIn).func_241113_a_(0, 12000, true, true);
+                        itemStack.damageItem(1, playerIn, p -> p.sendBreakAnimation(handIn));
+                        playerIn.getCooldownTracker().setCooldown(itemStack.getItem(), 6000);
 
-                    return new ActionResult<>(ActionResultType.SUCCESS, itemStack);
+                        flag = true;
+                    }
+
+                    if(flag){
+                        return new ActionResult<>(ActionResultType.SUCCESS, itemStack);
+                    }
                 }
             }
+
+            if(worldIn.isRemote) {
+                ItemStack off = playerIn.getHeldItemOffhand();
+                if (off.getItem() == Items.TORCH || off.getItem() == Items.WHEAT_SEEDS || off.getItem() == Items.GUNPOWDER) {
+                    flag = true;
+                }
+
+                if(flag) {
+                    Minecraft.getInstance().gameRenderer.displayItemActivation(new ItemStack(ItemRegistry.WEATHER_GEM.get()));
+                }
+            }
+
         }
 
         return new ActionResult<>(ActionResultType.FAIL, itemStack);
