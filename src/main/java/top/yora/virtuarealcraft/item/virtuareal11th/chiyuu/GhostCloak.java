e2f3890747c8f2a9a437b4c7a7826f3d060a9dc0
17146 <1714673995@qq.com>
2023-08-04 15:39:46 +0800

    添加了幽灵斗篷的功能

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal11th/chiyuu/GhostCloak.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal11th/chiyuu/GhostCloak.java
@@ -1,8 +1,14 @@
 package top.yora.virtuarealcraft.item.virtuareal11th.chiyuu;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -18,17 +24,38 @@ import java.util.List;
 
 public class GhostCloak extends Item {
     public GhostCloak() {
-        super(new Properties().group(ModGroup.itemgroup));
+        super(new Properties().maxDamage(21).group(ModGroup.itemgroup).maxStackSize(1));
     }
 
     @OnlyIn(Dist.CLIENT)
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        TooltipTool.addDevelopingText(tooltip);
-
         tooltip.add(new TranslationTextComponent("des.virtuarealcraft.ghost_cloak_1").mergeStyle(TextFormatting.GRAY));
         tooltip.add(new TranslationTextComponent("des.virtuarealcraft.ghost_cloak_2").mergeStyle(TextFormatting.GRAY).mergeStyle(TextFormatting.ITALIC));
 
         TooltipTool.addLiverInfo(tooltip, Livers.CHIYUU);
     }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+
+        if(!worldIn.isRemote){
+            playerIn.addPotionEffect(new EffectInstance(Effects.INVISIBILITY, 200, 0, false, false));
+            playerIn.addPotionEffect(new EffectInstance(Effects.STRENGTH, 200, 1, false, false));
+            playerIn.addPotionEffect(new EffectInstance(Effects.SPEED, 200, 1, false, false));
+
+            playerIn.getCooldownTracker().setCooldown(stack.getItem(), 400);
+
+            stack.damageItem(1, playerIn, player -> player.sendBreakAnimation(handIn));
+        }
+
+        return ActionResult.func_233538_a_(stack, worldIn.isRemote);
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+        return repair.getItem() == Items.LEATHER;
+    }
+
 }
