df67741c139a209597e340547885d41d81a160e9
17146 <1714673995@qq.com>
2023-08-06 01:24:50 +0800

    造轮子

--- src/main/java/top/yora/virtuarealcraft/tool/TooltipTool.java
+++ src/main/java/top/yora/virtuarealcraft/tool/TooltipTool.java
@@ -23,8 +23,62 @@ public class TooltipTool {
         String gen = liver.getGen();
         String bgColor = "#FFFFFF";
         switch (gen) {
+            case "1st":
+                bgColor = "#913837";
+                break;
             case "2nd":
-                bgColor = "#FF63A7";
+                bgColor = "#E8DED3";
+                break;
+            case "3rd":
+                bgColor = "#E6F4FD";
+                break;
+            case "4th":
+                bgColor = "#6ECECF";
+                break;
+            case "5th":
+                bgColor = "#EE756B";
+                break;
+            case "6th":
+                bgColor = "#6CDA90";
+                break;
+            case "7th":
+                bgColor = "#EF9EF7";
+                break;
+            case "8th":
+                bgColor = "#D8D382";
+                break;
+            case "9th":
+                bgColor = "#AC8582";
+                break;
+            case "10th":
+                bgColor = "#C9EDD8";
+                break;
+            case "11th":
+                bgColor = "#C67B7D";
+                break;
+            case "12th":
+                bgColor = "#F9F3DE";
+                break;
+            case "13th":
+                bgColor = "#F7CDDB";
+                break;
+            case "14th":
+                bgColor = "#AA9EE1";
+                break;
+            case "15th":
+                bgColor = "#B77361";
+                break;
+            case "16th":
+                bgColor = "#8DB4E8";
+                break;
+            case "17th":
+                bgColor = "#C92F51";
+                break;
+            case "18th":
+                bgColor = "#FCF9F0";
+                break;
+            case "19th":
+                bgColor = "#F8BCA3";
                 break;
         }
         if (!liver.isActive()) {
