b28b40e370f96e0b0594050615decd59479522ea
17146 <1714673995@qq.com>
2023-08-09 00:40:34 +0800

    微调

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal2nd/nanami/SharkTail.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal2nd/nanami/SharkTail.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 public class SharkTail extends ArmorItem {
     public SharkTail() {
-        super(ArmorMaterial.IRON, EquipmentSlotType.LEGS, new Properties().group(ModGroup.itemgroup).rarity(Rarity.UNCOMMON));
+        super(ArmorMaterial.IRON, EquipmentSlotType.LEGS, new Properties().group(ModGroup.itemgroup).rarity(Rarity.UNCOMMON).maxDamage(517));
     }
 
     @OnlyIn(Dist.CLIENT)
