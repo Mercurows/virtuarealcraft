d113df14feff89ea3bd2a4fa5444ee6b65c47dd7
17146 <1714673995@qq.com>
2023-08-09 00:16:55 +0800

    替换了鲨鱼尾巴的材质

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal2nd/nanami/SharkTail.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal2nd/nanami/SharkTail.java
@@ -4,11 +4,14 @@ import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -32,8 +35,6 @@ public class SharkTail extends ArmorItem {
     @OnlyIn(Dist.CLIENT)
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        TooltipTool.addDevelopingText(tooltip);
-
         tooltip.add(new TranslationTextComponent("des.virtuarealcraft.shark_tail").mergeStyle(TextFormatting.GRAY));
 
         TooltipTool.addLiverInfo(tooltip, Livers.NANAMI);
@@ -51,5 +52,16 @@ public class SharkTail extends ArmorItem {
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
         return Utils.MOD_ID + ":textures/models/armor/shark_tail.png";
+    }
+
+    @Override
+    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+        if(!world.isRemote){
+            if(player.isInWater()){
+                player.addPotionEffect(new EffectInstance(Effects.WATER_BREATHING, 40, 0, false, false));
+                player.addPotionEffect(new EffectInstance(Effects.DOLPHINS_GRACE, 40, 0, false, false));
+                player.addPotionEffect(new EffectInstance(Effects.CONDUIT_POWER, 40, 0, false, false));
+            }
+        }
     }
 }
