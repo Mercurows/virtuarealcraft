4923050dd1c93131bf8b755879b478455363e9f2
17146 <1714673995@qq.com>
2023-08-09 20:05:51 +0800

    添加了茉里宝珠的效果

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal11th/mari/BloodPearl.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal11th/mari/BloodPearl.java
@@ -1,21 +1,38 @@
 package top.yora.virtuarealcraft.item.virtuareal11th.mari;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import top.yora.virtuarealcraft.Utils;
 import top.yora.virtuarealcraft.group.ModGroup;
+import top.yora.virtuarealcraft.init.ItemRegistry;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.UUID;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class BloodPearl extends Item {
     public BloodPearl() {
         super(new Properties().group(ModGroup.itemgroup).maxStackSize(1));
@@ -24,12 +41,50 @@ public class BloodPearl extends Item {
     @OnlyIn(Dist.CLIENT)
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        TooltipTool.addDevelopingText(tooltip);
-
         tooltip.add(new TranslationTextComponent("des.virtuarealcraft.blood_pearl").mergeStyle(TextFormatting.GRAY));
 
         TooltipTool.addLiverInfo(tooltip, Livers.MARI);
     }
 
-    
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        if(!worldIn.isRemote && entityIn instanceof PlayerEntity){
+            PlayerEntity player = (PlayerEntity) entityIn;
+            if(itemSlot == 0){
+                player.addPotionEffect(new EffectInstance(Effects.JUMP_BOOST, 40, 0, false, false));
+            }
+        }
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> map = super.getAttributeModifiers(slot, stack);
+        UUID uuid = new UUID(ItemRegistry.BLOOD_PEARL.hashCode() + slot.toString().hashCode(), 0);
+        if (slot == EquipmentSlotType.OFFHAND) {
+            map = HashMultimap.create(map);
+            map.put(Attributes.MAX_HEALTH,
+                    new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, -0.3f, AttributeModifier.Operation.MULTIPLY_BASE));
+            map.put(Attributes.ATTACK_DAMAGE,
+                    new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, 0.2f, AttributeModifier.Operation.MULTIPLY_BASE));
+        }
+        return map;
+    }
+
+    @SubscribeEvent
+    public static void bloodPearlEffect(LivingDamageEvent event){
+        Entity entity = event.getSource().getImmediateSource();
+
+        if(entity instanceof PlayerEntity && !entity.world.isRemote){
+            PlayerEntity player = (PlayerEntity) entity;
+            if(player.getHeldItemOffhand().getItem() == ItemRegistry.BLOOD_PEARL.get()){
+                float damage = event.getAmount();
+
+                if(player.getHealth() < player.getMaxHealth() / 2){
+                    player.heal(damage * 0.22f);
+                }else {
+                    player.heal(damage * 0.11f);
+                }
+            }
+        }
+    }
 }
