2a4cbcf91eef2fc140c17da4e1b54f81a88a8bf9
17146 <1714673995@qq.com>
2023-08-21 23:52:38 +0800

    添加了弥的功能

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal5th/miki/Mi.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal5th/miki/Mi.java
@@ -1,17 +1,36 @@
 package top.yora.virtuarealcraft.item.virtuareal5th.miki;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import top.yora.virtuarealcraft.init.ItemRegistry;
+import top.yora.virtuarealcraft.tool.Livers;
+import top.yora.virtuarealcraft.tool.TooltipTool;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class Mi extends Item {
     public Mi() {
         super(new Properties());
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
+        tooltip.add(Component.translatable("des.virtuarealcraft.mi").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+
+        TooltipTool.addLiverInfo(tooltip, Livers.MIKI);
     }
 
     @Override
@@ -22,10 +41,11 @@ public class Mi extends Item {
             if(stack.getItem() == ItemRegistry.MIKI_TAIL.get()){
                 pPlayer.heal(1.0f);
                 mi.shrink(1);
+                pPlayer.getCooldowns().addCooldown(mi.getItem(), 10);
             }else {
                 pPlayer.addItem(new ItemStack(ItemRegistry.MI.get()));
+                pPlayer.getCooldowns().addCooldown(mi.getItem(), 20);
             }
-            pPlayer.getCooldowns().addCooldown(mi.getItem(), 20);
             return InteractionResultHolder.success(mi);
         }
         return InteractionResultHolder.fail(mi);
