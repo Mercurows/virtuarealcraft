0f2adeb2b9fa4af5696c5a4bdc25fb8b5a9b15eb
17146 <1714673995@qq.com>
2023-08-25 23:00:59 +0800

    添加了裤拉链的功能

--- src/main/java/top/yora/virtuarealcraft/block/KouichiZipper.java
+++ src/main/java/top/yora/virtuarealcraft/block/KouichiZipper.java
@@ -1,24 +1,81 @@
 package top.yora.virtuarealcraft.block;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
+@SuppressWarnings("deprecation")
 public class KouichiZipper extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public static final BooleanProperty OPEN = BooleanProperty.create("open");
 
     public KouichiZipper() {
-        super(Properties.of());
+        super(Properties.of().noCollission());
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(OPEN, false));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> tooltip, TooltipFlag pFlag) {
+        tooltip.add(Component.translatable("des.virtuarealcraft.kouichi_zipper").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY).withItalic(true)));
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(FACING, OPEN);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        BlockState blockstate = this.defaultBlockState();
+        blockstate = blockstate.setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+
+        return blockstate;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        if (pState.getValue(OPEN)) {
+            return Block.box(0, 0, 0, 16, 1, 16);
+        } else {
+            Direction facing = pState.getValue(FACING);
+            if(facing == Direction.SOUTH || facing == Direction.NORTH) {
+                return Block.box(7, 0, 0, 9, 1, 16);
+            }else {
+                return Block.box(0, 0, 7, 16, 1, 9);
+            }
+        }
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        pState = pState.cycle(OPEN);
+        pLevel.setBlock(pPos, pState, 2);
+//        this.playSound(pPlayer, pLevel, pPos, pState.getValue(OPEN));
+        return InteractionResult.sidedSuccess(pLevel.isClientSide);
     }
 }
