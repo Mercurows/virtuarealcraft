d08e957e3b325fa62a92bbe6cecd665db0456218
17146 <1714673995@qq.com>
2023-08-25 22:29:16 +0800

    添加了裤拉链的模型

--- src/main/java/top/yora/virtuarealcraft/block/KouichiZipper.java
+++ src/main/java/top/yora/virtuarealcraft/block/KouichiZipper.java
@@ -5,16 +5,16 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class KouichiZipper extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
-    public static final IntegerProperty OPEN = IntegerProperty.create("open", 0, 2);
+    public static final BooleanProperty OPEN = BooleanProperty.create("open");
 
     public KouichiZipper() {
         super(Properties.of());
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(OPEN, 0));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(OPEN, false));
     }
 
     @Override
