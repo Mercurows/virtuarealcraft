9419da14aa67a675065d7f79d49349c34dbc8b01
17146 <1714673995@qq.com>
2023-08-26 23:13:36 +0800

    添加了冰淇淋红茶的合成表

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal2nd/nanami/IceCreamBlackTea.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal2nd/nanami/IceCreamBlackTea.java
@@ -7,11 +7,9 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -48,5 +46,10 @@ public class IceCreamBlackTea extends Item {
     @Override
     public UseAnim getUseAnimation(ItemStack pStack) {
         return UseAnim.DRINK;
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
+        return new ItemStack(Items.BUCKET);
     }
 }
