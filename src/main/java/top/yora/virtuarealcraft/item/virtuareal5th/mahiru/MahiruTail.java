31fb73ddb78dcc69c47724cf405026cfbb8f81c0
17146 <1714673995@qq.com>
2023-09-05 19:30:38 +0800

    完成了尾巴的功能

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal5th/mahiru/MahiruTail.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal5th/mahiru/MahiruTail.java
@@ -1,24 +1,34 @@
 package top.yora.virtuarealcraft.item.virtuareal5th.mahiru;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import org.jetbrains.annotations.NotNull;
 import top.yora.virtuarealcraft.Utils;
+import top.yora.virtuarealcraft.init.ItemRegistry;
 import top.yora.virtuarealcraft.models.MihiruTailModel;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
@@ -27,17 +37,17 @@ import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class MahiruTail extends ArmorItem {
     public MahiruTail() {
-        super(ArmorMaterials.LEATHER, Type.LEGGINGS, new Properties());
+        super(ArmorMaterials.LEATHER, Type.LEGGINGS, new Properties().durability(200));
     }
 
     @OnlyIn(Dist.CLIENT)
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
-        TooltipTool.addDevelopingText(tooltip);
         tooltip.add(Component.translatable("des.virtuarealcraft.mahiru_tail_1").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY).withItalic(true)));
         tooltip.add(Component.translatable("des.virtuarealcraft.mahiru_tail_2").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
 
@@ -70,5 +80,41 @@ public class MahiruTail extends ArmorItem {
                 return armorModel;
             }
         });
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> map = super.getAttributeModifiers(slot, stack);
+        UUID uuid = new UUID(ItemRegistry.HAMSTER_WHEEL.hashCode() + slot.toString().hashCode(), 0);
+        if (slot == EquipmentSlot.LEGS) {
+            map = HashMultimap.create(map);
+            map.put(Attributes.MOVEMENT_SPEED,
+                    new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, 0.2f, AttributeModifier.Operation.MULTIPLY_BASE));
+            map.put(Attributes.ATTACK_SPEED,
+                    new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, 0.2f, AttributeModifier.Operation.MULTIPLY_BASE));
+        }
+        return map;
+    }
+
+    @Override
+    public void onArmorTick(ItemStack stack, Level level, Player player) {
+        if (!level.isClientSide) {
+            player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 300, 0, false, false));
+
+            boolean flag = false;
+            AABB box = new AABB(player.getOnPos()).inflate(10);
+            for (var entity : level.getEntitiesOfClass(Player.class, box)) {
+                if (!entity.equals(player)) {
+                    if (entity.getItemBySlot(EquipmentSlot.LEGS).getItem() == ItemRegistry.MIKI_TAIL.get()) {
+                        flag = true;
+                    }
+                }
+            }
+
+            if (flag) {
+                player.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 300, 0, false, false));
+                player.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, 300, 0, false, false));
+            }
+        }
     }
 }
