b4d980f45aa0e59191598276eaa19b085e6a2dab
17146 <1714673995@qq.com>
2023-09-08 02:02:29 +0800

    修复了造桥判定

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal6th/roi/RoyalHalo.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal6th/roi/RoyalHalo.java
@@ -18,10 +18,16 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
 import top.yora.virtuarealcraft.Utils;
 import top.yora.virtuarealcraft.block.CrystalBridgeBlock;
 import top.yora.virtuarealcraft.init.BlockRegistry;
+import top.yora.virtuarealcraft.init.ItemRegistry;
 import top.yora.virtuarealcraft.models.RoyalHaloModel;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
@@ -32,16 +38,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class RoyalHalo extends ArmorItem {
     public RoyalHalo() {
-        super(ArmorMaterials.GOLD, Type.HELMET, new Properties().rarity(Rarity.UNCOMMON));
+        super(ArmorMaterials.GOLD, Type.HELMET, new Properties().rarity(Rarity.UNCOMMON).durability(725));
     }
 
     @OnlyIn(Dist.CLIENT)
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
-        TooltipTool.addDevelopingText(tooltip);
-
         tooltip.add(Component.translatable("des.virtuarealcraft.royal_halo").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
 
         TooltipTool.addLiverInfo(tooltip, Livers.ROI);
@@ -49,24 +54,38 @@ public class RoyalHalo extends ArmorItem {
 
     @Override
     public void onArmorTick(ItemStack stack, Level level, Player player) {
-        if (!level.isClientSide && !player.isSteppingCarefully()) {
-            //TODO 修改为正确的造桥判定
-            BlockPos pos = player.getOnPosLegacy();
+        if (!level.isClientSide) {
+            BlockPos pos = player.getOnPos();
 
             BlockState state = level.getBlockState(pos);
 
-            if (state.getBlock() instanceof AirBlock) {
-                setBlocks(level, pos, BlockRegistry.CRYSTAL_BRIDGE.get().defaultBlockState());
-                stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(EquipmentSlot.HEAD));
-            } else if (state.getBlock() instanceof CrystalBridgeBlock) {
-                setBlocks(level, pos, BlockRegistry.CRYSTAL_BRIDGE.get().defaultBlockState());
+            if (!player.isSteppingCarefully() && player.onGround()) {
+                if (state.getBlock() instanceof AirBlock) {
+                    setBlocks(level, pos, BlockRegistry.CRYSTAL_BRIDGE.get().defaultBlockState());
+                    stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(EquipmentSlot.HEAD));
+                } else if (state.getBlock() instanceof CrystalBridgeBlock) {
+                    setBlocks(level, pos, BlockRegistry.CRYSTAL_BRIDGE.get().defaultBlockState());
+                }
+            } else {
+                if (state.getBlock() instanceof CrystalBridgeBlock) {
+                    level.removeBlock(pos, false);
+                }
+            }
+
+            if (player.fallDistance >= 0.5f && !player.isSteppingCarefully()) {
+                BlockState state1 = level.getBlockState(pos.offset(0, -1, 0));
+                if (state1.getBlock() instanceof AirBlock) {
+                    setBlocks(level, pos.offset(0, -1, 0), BlockRegistry.CRYSTAL_BRIDGE.get().defaultBlockState());
+                    stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(EquipmentSlot.HEAD));
+                }
             }
         }
-        super.onArmorTick(stack, level, player);
     }
 
     private static void setBlocks(Level level, BlockPos pos, BlockState state) {
-        level.setBlock(pos, state, 2);
+        if (level.getBlockState(pos).getBlock() instanceof AirBlock) {
+            level.setBlock(pos, state, 2);
+        }
         if (level.getBlockState(pos.offset(1, 0, 0)).getBlock() instanceof AirBlock) {
             level.setBlock(pos.offset(1, 0, 0), state, 2);
         }
@@ -107,5 +126,40 @@ public class RoyalHalo extends ArmorItem {
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
         return Utils.MOD_ID + ":textures/models/armor/royal_halo.png";
+    }
+
+    @SubscribeEvent
+    public static void onPlayerJump(LivingEvent.LivingJumpEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            ItemStack stack = player.getItemBySlot(EquipmentSlot.HEAD);
+
+            if (stack.getItem() == ItemRegistry.ROYAL_HALO.get() && !player.level().isClientSide) {
+                new Object() {
+                    private int ticks = 0;
+                    private float waitTicks;
+
+                    public void start(int waitTicks) {
+                        this.waitTicks = waitTicks;
+                        MinecraftForge.EVENT_BUS.register(this);
+                    }
+
+                    @SubscribeEvent
+                    public void tick(TickEvent.ServerTickEvent event) {
+                        if (event.phase == TickEvent.Phase.END) {
+                            this.ticks++;
+                            if (this.ticks >= this.waitTicks) {
+                                run();
+                            }
+                        }
+                    }
+
+                    private void run() {
+                        setBlocks(player.level(), player.getOnPos().offset(0, -1, 0), BlockRegistry.CRYSTAL_BRIDGE.get().defaultBlockState());
+                        stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(EquipmentSlot.HEAD));
+                        MinecraftForge.EVENT_BUS.unregister(this);
+                    }
+                }.start(3);
+            }
+        }
     }
 }
