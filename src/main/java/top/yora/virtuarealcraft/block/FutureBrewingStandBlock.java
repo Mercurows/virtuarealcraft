9cf04d40e063f084a2c900678cb4336d18eb6115
17146 <1714673995@qq.com>
2023-09-11 22:03:14 +0800

    添加了部分炼药台方法

--- src/main/java/top/yora/virtuarealcraft/block/FutureBrewingStandBlock.java
+++ src/main/java/top/yora/virtuarealcraft/block/FutureBrewingStandBlock.java
@@ -1,10 +1,41 @@
 package top.yora.virtuarealcraft.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+import top.yora.virtuarealcraft.block.blockentity.FutureBrewingStandBlockEntity;
+import top.yora.virtuarealcraft.init.BlockEntityRegistry;
 
-public class FutureBrewingStandBlock extends Block {
+public class FutureBrewingStandBlock extends Block implements EntityBlock {
     public FutureBrewingStandBlock() {
         super(BlockBehaviour.Properties.of().strength(4.5f));
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return new FutureBrewingStandBlockEntity(pPos, pState);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        if (!pLevel.isClientSide) {
+            return createTickerHelper(pBlockEntityType, BlockEntityRegistry.FUTURE_BREWING_STAND_BLOCK_ENTITY.get(), FutureBrewingStandBlockEntity::serverTick);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Nullable
+    protected static <E extends BlockEntity, A extends BlockEntity> BlockEntityTicker<A> createTickerHelper(BlockEntityType<A> pServerType, BlockEntityType<E> pClientType, BlockEntityTicker<? super E> pTicker) {
+        return pClientType == pServerType ? (BlockEntityTicker<A>) pTicker : null;
     }
 }
--- src/main/java/top/yora/virtuarealcraft/block/blockentity/FutureBrewingStandBlockEntity.java
+++ src/main/java/top/yora/virtuarealcraft/block/blockentity/FutureBrewingStandBlockEntity.java
@@ -3,6 +3,7 @@ package top.yora.virtuarealcraft.block.blockentity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
@@ -10,29 +11,61 @@ import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import top.yora.virtuarealcraft.init.BlockEntityRegistry;
+import top.yora.virtuarealcraft.init.ItemRegistry;
 
 public class FutureBrewingStandBlockEntity extends BaseContainerBlockEntity implements WorldlyContainer {
+    /**
+     * 原料槽 - 6
+     * 燃料槽 - 7
+     * 粉末槽 - 8 （用于放置红石粉/荧石粉/时守星沙等）
+     */
     private static final int INGREDIENT_SLOT = 6;
     private static final int FUEL_SLOT = 7;
+    private static final int POWDER_SLOT = 8;
+
+    /**
+     * 顶部输入 - 原材料
+     * 底部输出 - 原材料和药水
+     * 侧面输入 - 燃料、粉末和药水
+     */
     private static final int[] SLOTS_FOR_UP = new int[]{6};
-    //水瓶和材料
     private static final int[] SLOTS_FOR_DOWN = new int[]{0, 1, 2, 3, 4, 5, 6};
-    //水瓶和燃料
-    private static final int[] SLOTS_FOR_SIDES = new int[]{0, 1, 2, 3, 4, 5, 7};
+    private static final int[] SLOTS_FOR_SIDES = new int[]{0, 1, 2, 3, 4, 5, 7, 8};
+
     public static final int FUEL_USES = 20;
     public static final int DATA_BREW_TIME = 0;
     public static final int DATA_FUEL_USES = 1;
     public static final int NUM_DATA_VALUES = 2;
 
-    private NonNullList<ItemStack> items = NonNullList.withSize(5, ItemStack.EMPTY);
+    private NonNullList<ItemStack> items = NonNullList.withSize(9, ItemStack.EMPTY);
+
+    int brewTime;
+    private boolean[] lastPotionCount;
+    private Item ingredient;
+    int fuel;
+
+    /**
+     * 模式：
+     * 0 - 正常
+     * 1 - 自动注水
+     * 2 - 自动注水 + 自动粗制
+     */
+    int mode;
 
     public FutureBrewingStandBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntityRegistry.FUTURE_BREWING_STAND_BLOCK_ENTITY.get(), pos, state);
+    }
+
+    public static void serverTick(Level pLevel, BlockPos pPos, BlockState pState, FutureBrewingStandBlockEntity pBlockEntity) {
+        //TODO 完成tick方法
     }
 
     @Override
@@ -61,6 +94,7 @@ public class FutureBrewingStandBlockEntity extends BaseContainerBlockEntity impl
 
     @Override
     protected AbstractContainerMenu createMenu(int pContainerId, Inventory pInventory) {
+        //TODO 完成炼药台的GUI
         return null;
     }
 
@@ -76,8 +110,52 @@ public class FutureBrewingStandBlockEntity extends BaseContainerBlockEntity impl
                 return false;
             }
         }
-
         return true;
+    }
+
+    private boolean[] getPotionBits() {
+        boolean[] bool = new boolean[3];
+
+        for(int i = 0; i < 6; ++i) {
+            if (!this.items.get(i).isEmpty()) {
+                bool[i] = true;
+            }
+        }
+
+        return bool;
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        ContainerHelper.loadAllItems(pTag, this.items);
+        this.brewTime = pTag.getShort("BrewTime");
+        this.fuel = pTag.getByte("Fuel");
+        this.mode = pTag.getShort("Mode");
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+        pTag.putShort("BrewTime", (short)this.brewTime);
+        ContainerHelper.saveAllItems(pTag, this.items);
+        pTag.putByte("Fuel", (byte)this.fuel);
+        pTag.putShort("Mode", (short)this.mode);
+    }
+
+    @Override
+    public boolean canPlaceItem(int pIndex, ItemStack pStack) {
+        //TODO 修改为自定义的配方检测
+        if (pIndex == 6) {
+            return net.minecraftforge.common.brewing.BrewingRecipeRegistry.isValidIngredient(pStack);
+        } else if (pIndex == 7) {
+            return pStack.is(Items.BLAZE_POWDER);
+        } else if (pIndex == 8) {
+            return pStack.is(Items.REDSTONE) || pStack.is(Items.GLOWSTONE_DUST) || pStack.is(ItemRegistry.TOKIMORI_SEISAND.get());
+        } else {
+            return net.minecraftforge.common.brewing.BrewingRecipeRegistry.isValidInput(pStack) && this.getItem(pIndex).isEmpty();
+        }
     }
 
     @Override
@@ -97,7 +175,9 @@ public class FutureBrewingStandBlockEntity extends BaseContainerBlockEntity impl
 
     @Override
     public void setItem(int pSlot, ItemStack pStack) {
-
+        if (pSlot >= 0 && pSlot < this.items.size()) {
+            this.items.set(pSlot, pStack);
+        }
     }
 
     @Override
