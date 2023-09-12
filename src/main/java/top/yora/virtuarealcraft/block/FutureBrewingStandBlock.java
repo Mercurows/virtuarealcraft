f50070aa4f3488beec8232ebc7943dbedadb7977
17146 <1714673995@qq.com>
2023-09-12 21:03:28 +0800

    添加了炼药台的模型

--- src/main/java/top/yora/virtuarealcraft/block/FutureBrewingStandBlock.java
+++ src/main/java/top/yora/virtuarealcraft/block/FutureBrewingStandBlock.java
@@ -1,21 +1,32 @@
 package top.yora.virtuarealcraft.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 import top.yora.virtuarealcraft.block.blockentity.FutureBrewingStandBlockEntity;
 import top.yora.virtuarealcraft.init.BlockEntityRegistry;
 
 public class FutureBrewingStandBlock extends Block implements EntityBlock {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
     public FutureBrewingStandBlock() {
-        super(BlockBehaviour.Properties.of().strength(4.5f));
+        super(BlockBehaviour.Properties.of().strength(4.5f).noParticlesOnBreak().noOcclusion());
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Nullable
@@ -37,5 +48,22 @@ public class FutureBrewingStandBlock extends Block implements EntityBlock {
     @Nullable
     protected static <E extends BlockEntity, A extends BlockEntity> BlockEntityTicker<A> createTickerHelper(BlockEntityType<A> pServerType, BlockEntityType<E> pClientType, BlockEntityTicker<? super E> pTicker) {
         return pClientType == pServerType ? (BlockEntityTicker<A>) pTicker : null;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return Block.box(3, 0, 3, 13, 10, 13);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        BlockState blockstate = this.defaultBlockState();
+        blockstate = blockstate.setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+        return blockstate;
     }
 }
