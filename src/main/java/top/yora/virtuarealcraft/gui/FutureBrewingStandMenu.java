d8ef8693fecb4d0217828b297ad3839e4af3671d
17146 <1714673995@qq.com>
2023-09-14 02:45:15 +0800

    添加了炼药台的部分逻辑

--- src/main/java/top/yora/virtuarealcraft/gui/FutureBrewingStandMenu.java
+++ src/main/java/top/yora/virtuarealcraft/gui/FutureBrewingStandMenu.java
@@ -19,6 +19,7 @@ public class FutureBrewingStandMenu extends AbstractContainerMenu {
     private final Container brewingStand;
     private final ContainerData brewingStandData;
     private final Slot ingredientSlot;
+    private final Slot powderSlot;
 
     public FutureBrewingStandMenu(int pContainerId, Inventory pPlayerInventory) {
         this(pContainerId, pPlayerInventory, new SimpleContainer(9), new SimpleContainerData(2));
@@ -40,16 +41,16 @@ public class FutureBrewingStandMenu extends AbstractContainerMenu {
         this.addSlot(new FutureBrewingStandMenu.PotionSlot(pBrewingStandContainer, 5, 156, 51));
         this.ingredientSlot = this.addSlot(new FutureBrewingStandMenu.IngredientsSlot(pBrewingStandContainer, 6, 96, 17));
         this.addSlot(new FutureBrewingStandMenu.FuelSlot(pBrewingStandContainer, 7, 17, 17));
-        this.addSlot(new FutureBrewingStandMenu.PowderSlot(pBrewingStandContainer, 8, 120, 17));
+        this.powderSlot = this.addSlot(new FutureBrewingStandMenu.PowderSlot(pBrewingStandContainer, 8, 120, 17));
         this.addDataSlots(pBrewingStandData);
 
-        for(int i = 0; i < 3; ++i) {
-            for(int j = 0; j < 9; ++j) {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 9; ++j) {
                 this.addSlot(new Slot(pPlayerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
             }
         }
 
-        for(int k = 0; k < 9; ++k) {
+        for (int k = 0; k < 9; ++k) {
             this.addSlot(new Slot(pPlayerInventory, k, 8 + k * 18, 142));
         }
 
@@ -57,8 +58,66 @@ public class FutureBrewingStandMenu extends AbstractContainerMenu {
 
     @Override
     public ItemStack quickMoveStack(Player pPlayer, int pIndex) {
-        //TODO 完成快速填充方法
-        return null;
+        //TODO 完成从原料槽和粉末槽拿回物品的操作
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(pIndex);
+        if (slot.hasItem()) {
+            ItemStack itemstack1 = slot.getItem();
+            itemstack = itemstack1.copy();
+            if ((pIndex < 0 || pIndex > 2) && pIndex != 3 && pIndex != 4) {
+                if (FutureBrewingStandMenu.FuelSlot.mayPlaceItem(itemstack)) {
+                    //TODO 修改为正确的逻辑：如果燃料槽为空，则直接放入燃料槽；如果燃料槽不空，原料槽为空，则放入原料槽，否则放不进去
+                    if (this.moveItemStackTo(itemstack1, 7, 8, false) ||
+                            !this.moveItemStackTo(itemstack1, 9, 45, false) ||
+                            this.ingredientSlot.mayPlace(itemstack1) && !this.moveItemStackTo(itemstack1, 6, 7, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (this.powderSlot.mayPlace(itemstack1)) {
+                    if (this.moveItemStackTo(itemstack1, 8, 9, false) ||
+                            this.ingredientSlot.mayPlace(itemstack1) && !this.moveItemStackTo(itemstack1, 6, 7, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (this.ingredientSlot.mayPlace(itemstack1)) {
+                    if (!this.moveItemStackTo(itemstack1, 6, 7, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (FutureBrewingStandMenu.PotionSlot.mayPlaceItem(itemstack)) {
+                    if (!this.moveItemStackTo(itemstack1, 0, 6, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (pIndex >= 9 && pIndex < 36) {
+                    if (!this.moveItemStackTo(itemstack1, 36, 45, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (pIndex >= 36 && pIndex < 45) {
+                    if (!this.moveItemStackTo(itemstack1, 9, 36, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (!this.moveItemStackTo(itemstack1, 9, 45, false)) {
+                    return ItemStack.EMPTY;
+                }
+            } else {
+                if (!this.moveItemStackTo(itemstack1, 9, 45, true)) {
+                    return ItemStack.EMPTY;
+                }
+
+                slot.onQuickCraft(itemstack1, itemstack);
+            }
+
+            if (itemstack1.isEmpty()) {
+                slot.setByPlayer(ItemStack.EMPTY);
+            } else {
+                slot.setChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(pPlayer, itemstack1);
+        }
+
+        return itemstack;
     }
 
     @Override
@@ -129,7 +188,7 @@ public class FutureBrewingStandMenu extends AbstractContainerMenu {
             Potion potion = PotionUtils.getPotion(pStack);
             if (pPlayer instanceof ServerPlayer) {
                 net.minecraftforge.event.ForgeEventFactory.onPlayerBrewedPotion(pPlayer, pStack);
-                CriteriaTriggers.BREWED_POTION.trigger((ServerPlayer)pPlayer, potion);
+                CriteriaTriggers.BREWED_POTION.trigger((ServerPlayer) pPlayer, potion);
             }
 
             super.onTake(pPlayer, pStack);
