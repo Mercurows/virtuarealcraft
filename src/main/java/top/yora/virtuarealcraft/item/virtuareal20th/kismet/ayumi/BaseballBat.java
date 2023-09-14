f193e2407a95737583775d2eea09ff546891b564
17146 <1714673995@qq.com>
2023-09-15 02:50:59 +0800

    添加了棒球棒的模型和功能

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal20th/kismet/ayumi/BaseballBat.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal20th/kismet/ayumi/BaseballBat.java
@@ -3,11 +3,13 @@ package top.yora.virtuarealcraft.item.virtuareal20th.kismet.ayumi;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import top.yora.virtuarealcraft.tool.Livers;
@@ -18,15 +20,22 @@ import java.util.List;
 
 public class BaseballBat extends SwordItem {
     public BaseballBat() {
-        super(Tiers.WOOD, 1, -2.6f, new Properties().durability(207));
+        super(Tiers.WOOD, 3, -2.6f, new Properties().durability(207));
     }
 
     @OnlyIn(Dist.CLIENT)
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
-        TooltipTool.addDevelopingText(tooltip);
-        tooltip.add(Component.translatable("des.virtuarealcraft.baseball_bat").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        tooltip.add(Component.translatable("des.virtuarealcraft.baseball_bat_1").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        tooltip.add(Component.translatable("des.virtuarealcraft.baseball_bat_2").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY).withItalic(true)));
 
         TooltipTool.addLiverInfo(tooltip, Livers.AYUMI);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        pTarget.addDeltaMovement(new Vec3(pTarget.getX() - pAttacker.getX(), 0, pTarget.getZ() - pAttacker.getZ())
+                .normalize().add(0, 0.5, 0).scale(5));
+        return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
 }
