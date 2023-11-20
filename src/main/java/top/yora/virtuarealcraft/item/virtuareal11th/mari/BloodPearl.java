ee90c71fc6ddd9c5caea8beed7ef68d1a9fd1d29
17146 <1714673995@qq.com>
2023-11-20 21:04:54 +0800

    重做了祭血宝珠效果

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal11th/mari/BloodPearl.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal11th/mari/BloodPearl.java
@@ -8,7 +8,7 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -22,6 +22,9 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
 import top.yora.virtuarealcraft.Utils;
 import top.yora.virtuarealcraft.init.ItemRegistry;
 import top.yora.virtuarealcraft.tool.Livers;
@@ -30,9 +33,10 @@ import top.yora.virtuarealcraft.tool.TooltipTool;
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class BloodPearl extends Item {
+public class BloodPearl extends Item implements ICurioItem {
     public BloodPearl() {
         super(new Properties().stacksTo(1));
     }
@@ -46,26 +50,33 @@ public class BloodPearl extends Item {
     }
 
     @Override
-    public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-        if (!worldIn.isClientSide && entityIn instanceof Player player) {
-            if (itemSlot == 0) {
-                player.addEffect(new MobEffectInstance(MobEffects.JUMP, 40, 0, false, false));
-            }
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        LivingEntity livingEntity = slotContext.entity();
+        if (!livingEntity.level().isClientSide) {
+            livingEntity.addEffect(new MobEffectInstance(MobEffects.JUMP, 40, 0, false, false));
         }
     }
 
     @Override
-    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
-        Multimap<Attribute, AttributeModifier> map = super.getAttributeModifiers(slot, stack);
-        UUID uuid = new UUID(ItemRegistry.BLOOD_PEARL.hashCode() + slot.toString().hashCode(), 0);
-        if (slot == EquipmentSlot.OFFHAND) {
-            map = HashMultimap.create(map);
-            map.put(Attributes.MAX_HEALTH,
-                    new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, -0.3f, AttributeModifier.Operation.MULTIPLY_BASE));
-            map.put(Attributes.ATTACK_DAMAGE,
-                    new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, 0.2f, AttributeModifier.Operation.MULTIPLY_BASE));
-        }
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> map = ICurioItem.super.getAttributeModifiers(slotContext, uuid, stack);
+
+        map = HashMultimap.create(map);
+        map.put(Attributes.MAX_HEALTH,
+                new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, -0.3f, AttributeModifier.Operation.MULTIPLY_BASE));
+        map.put(Attributes.ATTACK_DAMAGE,
+                new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, 0.2f, AttributeModifier.Operation.MULTIPLY_BASE));
+
         return map;
+    }
+
+    @Override
+    public boolean canEquip(SlotContext slotContext, ItemStack stack) {
+        LivingEntity livingEntity = slotContext.entity();
+        AtomicBoolean flag = new AtomicBoolean(true);
+        CuriosApi.getCuriosInventory(livingEntity).ifPresent(c -> c.findFirstCurio(this).ifPresent(s -> flag.set(false)));
+
+        return flag.get();
     }
 
     @SubscribeEvent
@@ -73,15 +84,17 @@ public class BloodPearl extends Item {
         Entity entity = event.getSource().getDirectEntity();
 
         if (entity instanceof Player player && !entity.level().isClientSide) {
-            if (player.getOffhandItem().getItem() == ItemRegistry.BLOOD_PEARL.get()) {
-                float damage = event.getAmount();
+            CuriosApi.getCuriosInventory(player).ifPresent(s -> s.findFirstCurio(ItemRegistry.BLOOD_PEARL.get()).ifPresent(
+                    r -> {
+                        float damage = event.getAmount();
 
-                if (player.getHealth() < player.getMaxHealth() / 2) {
-                    player.heal(damage * 0.4f);
-                } else {
-                    player.heal(damage * 0.2f);
-                }
-            }
+                        if (player.getHealth() < player.getMaxHealth() / 2) {
+                            player.heal(damage * 0.4f);
+                        } else {
+                            player.heal(damage * 0.2f);
+                        }
+                    }
+            ));
         }
     }
 }
