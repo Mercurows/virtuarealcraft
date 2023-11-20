8b49f67df8f530eb4dc0fe40686e115e986f0dec
17146 <1714673995@qq.com>
2023-11-20 21:17:33 +0800

    重做了幽灵斗篷效果

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal11th/chiyuu/GhostCloak.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal11th/chiyuu/GhostCloak.java
@@ -3,27 +3,28 @@ package top.yora.virtuarealcraft.item.virtuareal11th.chiyuu;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class GhostCloak extends Item {
+public class GhostCloak extends Item implements ICurioItem {
     public GhostCloak() {
-        super(new Properties().durability(21));
+        super(new Properties().stacksTo(1));
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -36,25 +37,21 @@ public class GhostCloak extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
-        ItemStack stack = playerIn.getItemInHand(handIn);
-
-        if (!worldIn.isClientSide) {
-            playerIn.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 200, 0, false, false));
-            playerIn.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 1, false, false));
-            playerIn.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 1, false, false));
-
-            playerIn.getCooldowns().addCooldown(stack.getItem(), 400);
-
-            stack.hurtAndBreak(1, playerIn, player -> player.broadcastBreakEvent(handIn));
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        LivingEntity livingEntity = slotContext.entity();
+        if (!livingEntity.level().isClientSide) {
+            livingEntity.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 40, 0, false, false));
+            livingEntity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 0, false, false));
+            livingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 0, false, false));
         }
-
-        return InteractionResultHolder.sidedSuccess(stack, worldIn.isClientSide);
     }
 
     @Override
-    public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair) {
-        return repair.getItem() == Items.LEATHER;
-    }
+    public boolean canEquip(SlotContext slotContext, ItemStack stack) {
+        LivingEntity livingEntity = slotContext.entity();
+        AtomicBoolean flag = new AtomicBoolean(true);
+        CuriosApi.getCuriosInventory(livingEntity).ifPresent(c -> c.findFirstCurio(this).ifPresent(s -> flag.set(false)));
 
+        return flag.get();
+    }
 }
--- src/main/java/top/yora/virtuarealcraft/item/virtuareal16th/kiyuu/GameConsole.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal16th/kiyuu/GameConsole.java
@@ -9,6 +9,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -18,6 +19,7 @@ import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import top.yora.virtuarealcraft.init.ItemRegistry;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
 
@@ -50,7 +52,7 @@ public class GameConsole extends Item {
             double prob = Math.min(0.3 + level * 0.01, 0.8);
             double rand = Math.random();
 
-            if (rand < prob) {
+            if (rand < prob || player.getItemBySlot(EquipmentSlot.HEAD).getItem() == ItemRegistry.TACTICAL_HEADSET_MK1.get()) {
                 if (!pLevel.isClientSide) {
                     player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2400, 1));
                     player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 2400, 1));
