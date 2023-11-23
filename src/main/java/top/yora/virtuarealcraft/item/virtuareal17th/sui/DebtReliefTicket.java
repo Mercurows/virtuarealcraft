e6534bb3dbbb3ba51a59ffd8f0a68d0422134b9e
17146 <1714673995@qq.com>
2023-11-24 02:34:08 +0800

    更新了债务免除券的功能

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal17th/sui/DebtReliefTicket.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal17th/sui/DebtReliefTicket.java
@@ -4,6 +4,8 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.npc.VillagerProfession;
@@ -44,6 +46,7 @@ public class DebtReliefTicket extends Item {
                 }
 
                 villager.restock();
+                pPlayer.addEffect(new MobEffectInstance(MobEffects.HERO_OF_THE_VILLAGE, 1200, 4));
 
                 if (!pPlayer.isCreative()) {
                     pStack.shrink(1);
