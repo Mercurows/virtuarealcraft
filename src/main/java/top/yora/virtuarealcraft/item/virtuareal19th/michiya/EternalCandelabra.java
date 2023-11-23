1325b3b7cccc26c2a75c15f4a35d6896154c0d6f
17146 <1714673995@qq.com>
2023-11-24 00:04:39 +0800

    添加了永夜烛台的配方和攻击动作

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal19th/michiya/EternalCandelabra.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal19th/michiya/EternalCandelabra.java
@@ -1,7 +1,15 @@
 package top.yora.virtuarealcraft.item.virtuareal19th.michiya;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
@@ -9,23 +17,49 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ForgeMod;
+import top.yora.virtuarealcraft.Utils;
+import top.yora.virtuarealcraft.init.ItemRegistry;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.UUID;
 
 public class EternalCandelabra extends SwordItem {
     public EternalCandelabra() {
-        super(Tiers.IRON, 4, -2.5f, new Properties().durability(1118));
+        super(Tiers.IRON, 4, -2.8f, new Properties().durability(1118));
     }
 
     @OnlyIn(Dist.CLIENT)
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
-        TooltipTool.addDevelopingText(tooltip);
         tooltip.add(Component.translatable("des.virtuarealcraft.eternal_candelabra").withStyle(ChatFormatting.GRAY));
 
         TooltipTool.addLiverInfo(tooltip, Livers.MICHIYA);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        if (!pAttacker.level().isClientSide) {
+            pTarget.setSecondsOnFire(380);
+            pTarget.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 1));
+            pTarget.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 100, 0));
+        }
+
+        return super.hurtEnemy(pStack, pTarget, pAttacker);
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot pEquipmentSlot) {
+        Multimap<Attribute, AttributeModifier> map = super.getDefaultAttributeModifiers(pEquipmentSlot);
+        UUID uuid = new UUID(ItemRegistry.HAMSTER_WHEEL.hashCode() + pEquipmentSlot.toString().hashCode(), 0);
+        if (pEquipmentSlot == EquipmentSlot.MAINHAND) {
+            map = HashMultimap.create(map);
+            map.put(ForgeMod.ENTITY_REACH.get(),
+                    new AttributeModifier(uuid, Utils.ATTRIBUTE_MODIFIER, 2.0f, AttributeModifier.Operation.ADDITION));
+        }
+        return map;
     }
 }
