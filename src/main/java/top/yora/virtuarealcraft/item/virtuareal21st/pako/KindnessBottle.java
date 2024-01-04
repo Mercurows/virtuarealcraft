b9940917cb29d6571b8a4ea6eff642bf3ac439c6
17146 <1714673995@qq.com>
2024-01-05 01:35:56 +0800

    添加access transformer

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal21st/pako/KindnessBottle.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal21st/pako/KindnessBottle.java
@@ -2,6 +2,12 @@ package top.yora.virtuarealcraft.item.virtuareal21st.pako;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.ZombieVillager;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -26,5 +32,26 @@ public class KindnessBottle extends Item {
         tooltip.add(Component.translatable("des.virtuarealcraft.kindness_bottle").withStyle(ChatFormatting.GRAY));
 
         TooltipTool.addLiverInfo(tooltip, Livers.PAKO);
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
+        if (pInteractionTarget instanceof ZombieVillager zombieVillager) {
+            if (zombieVillager.hasEffect(MobEffects.WEAKNESS)) {
+                if (!pPlayer.getAbilities().instabuild) {
+                    pStack.setDamageValue(pStack.getMaxDamage());
+                }
+
+                if (!pPlayer.level().isClientSide) {
+                    zombieVillager.startConverting(pPlayer.getUUID(), 200);
+                }
+
+                return InteractionResult.SUCCESS;
+            } else {
+                return InteractionResult.CONSUME;
+            }
+        }
+
+        return super.interactLivingEntity(pStack, pPlayer, pInteractionTarget, pUsedHand);
     }
 }
