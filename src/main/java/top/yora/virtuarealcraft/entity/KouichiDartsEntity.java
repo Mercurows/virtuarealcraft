ad826a6661a56fe420a3042fc93865a8fdc066cb
17146 <1714673995@qq.com>
2024-01-05 03:11:30 +0800

    移植了血羽魔翼的闪烁功能

--- src/main/java/top/yora/virtuarealcraft/entity/KouichiDartsEntity.java
+++ src/main/java/top/yora/virtuarealcraft/entity/KouichiDartsEntity.java
@@ -1,14 +1,16 @@
 package top.yora.virtuarealcraft.entity;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import top.yora.virtuarealcraft.init.EntityRegistry;
 import top.yora.virtuarealcraft.init.ItemRegistry;
 
@@ -33,8 +35,29 @@ public class KouichiDartsEntity extends ThrowableItemProjectile {
     }
 
     @Override
-    protected void onHitBlock(BlockHitResult pResult) {
-        this.discard();
+    protected void onHit(HitResult pResult) {
+        super.onHit(pResult);
+
+        if (!this.level().isClientSide && !this.isRemoved()) {
+            Entity entity = this.getOwner();
+            if (entity instanceof ServerPlayer serverplayer) {
+                if (serverplayer.connection.isAcceptingMessages() && serverplayer.level() == this.level() && !serverplayer.isSleeping() && serverplayer.isSteppingCarefully()) {
+                    if (entity.isPassenger()) {
+                        serverplayer.dismountTo(this.getX(), this.getY(), this.getZ());
+                    } else {
+                        entity.teleportTo(this.getX(), this.getY(), this.getZ());
+                    }
+
+                    entity.teleportTo(this.getX(), this.getY(), this.getZ());
+                    entity.resetFallDistance();
+                }
+            } else if (entity != null) {
+                entity.teleportTo(this.getX(), this.getY(), this.getZ());
+                entity.resetFallDistance();
+            }
+
+            this.discard();
+        }
     }
 
     @Override
