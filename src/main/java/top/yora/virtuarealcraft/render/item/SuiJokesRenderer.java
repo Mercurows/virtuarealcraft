1083f3e3c255a2d53d7cf979ce92099f1995371b
17146 <1714673995@qq.com>
2024-01-14 05:28:13 +0800

    添加部分渲染效果

--- src/main/java/top/yora/virtuarealcraft/render/item/SuiJokesRenderer.java
+++ src/main/java/top/yora/virtuarealcraft/render/item/SuiJokesRenderer.java
@@ -3,7 +3,7 @@ package top.yora.virtuarealcraft.render.item;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
@@ -53,11 +53,56 @@ public class SuiJokesRenderer extends BlockEntityWithoutLevelRenderer {
         stack.scale(0.6F, 0.6F, 0.6F);
         stack.popPose();
 
-        LocalPlayer player = Minecraft.getInstance().player;
+        // Font
+        stack.pushPose();
+        Font font = Minecraft.getInstance().font;
+        stack.mulPose(Axis.XN.rotationDegrees(-120.0F));
 
         stack.pushPose();
+        stack.scale(.0035f, .0035f, .0035f);
+        stack.translate(-45, -85, -5);
+
+        if (pDisplayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND) {
+            stack.translate(10, 0, 0);
+        }
+        font.drawInBatch("令人忍俊不禁的", 0, 0, 0xdcb77f, false, stack.last().pose(), pBuffer, Font.DisplayMode.NORMAL, 0, pPackedLight);
+        stack.popPose();
+
+        stack.pushPose();
+        stack.scale(.01f, .01f, .01f);
+
+        stack.translate(-22, -25, -5);
+
+        if (pDisplayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND) {
+            stack.translate(4.5, 0, 0);
+        }
+        font.drawInBatch("岁己笑话", 0, 0, 0xfacddf, false, stack.last().pose(), pBuffer, Font.DisplayMode.NORMAL, 0, pPackedLight);
+        stack.popPose();
+
+        stack.pushPose();
+        stack.scale(.02f, .02f, .02f);
+
+        stack.translate(-11, -8, -5);
+
+        if (pDisplayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND) {
+            stack.translate(2.5, 0, 0);
+        }
+        font.drawInBatch("大全", 0, 0, 0xf3c0d4, false, stack.last().pose(), pBuffer, Font.DisplayMode.NORMAL, 0, pPackedLight);
+        stack.popPose();
+
+        stack.pushPose();
+        stack.scale(.0045f, .0045f, .0045f);
+        stack.translate(-30, 20, -5);
+
+        if (pDisplayContext == ItemDisplayContext.FIRST_PERSON_LEFT_HAND) {
+            stack.translate(8, 0, 0);
+        }
+        font.drawInBatch("小黑饼 著", 0, 0, 0x000000, false, stack.last().pose(), pBuffer, Font.DisplayMode.NORMAL, 0, pPackedLight);
+        stack.popPose();
+
 
         stack.popPose();
+
 
         stack.popPose();
     }
