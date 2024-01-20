c16f2dc4da3982db2cddb277fe81c4aa8e2b550a
17146 <1714673995@qq.com>
2024-01-20 15:23:21 +0800

    添加灼天宝鉴的功能

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal21st/yukisyo/BurningMirror.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal21st/yukisyo/BurningMirror.java
@@ -2,19 +2,27 @@ package top.yora.virtuarealcraft.item.virtuareal21st.yukisyo;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
 import top.yora.virtuarealcraft.tool.Livers;
 import top.yora.virtuarealcraft.tool.TooltipTool;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class BurningMirror extends Item {
+public class BurningMirror extends Item implements ICurioItem {
     public BurningMirror() {
         super(new Properties());
     }
@@ -26,5 +34,29 @@ public class BurningMirror extends Item {
         tooltip.add(Component.translatable("des.virtuarealcraft.burning_mirror").withStyle(ChatFormatting.GRAY));
 
         TooltipTool.addLiverInfo(tooltip, Livers.YUKISYO);
+    }
+
+    @Override
+    public boolean canEquip(SlotContext slotContext, ItemStack stack) {
+        LivingEntity livingEntity = slotContext.entity();
+        AtomicBoolean flag = new AtomicBoolean(true);
+        CuriosApi.getCuriosInventory(livingEntity).ifPresent(c -> c.findFirstCurio(this).ifPresent(s -> flag.set(false)));
+
+        return flag.get();
+    }
+
+    @Override
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        LivingEntity entity = slotContext.entity();
+
+        List<MobEffectInstance> effectInstances = new ArrayList<>();
+
+        entity.getActiveEffects().forEach(e -> {
+            if (e.getEffect().getCategory() != MobEffectCategory.BENEFICIAL && e.getAmplifier() <= 1) {
+                effectInstances.add(e);
+            }
+        });
+
+        effectInstances.forEach(e -> entity.removeEffect(e.getEffect()));
     }
 }
