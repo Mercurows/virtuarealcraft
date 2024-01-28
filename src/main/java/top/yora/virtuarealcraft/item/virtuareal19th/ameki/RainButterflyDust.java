7d049d47313a800bb9d370f8de47a1616d5a47f2
17146 <1714673995@qq.com>
2024-01-28 19:25:24 +0800

    添加了新材料的名称和描述

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal19th/ameki/RainButterflyDust.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal19th/ameki/RainButterflyDust.java
@@ -1,9 +1,26 @@
 package top.yora.virtuarealcraft.item.virtuareal19th.ameki;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import top.yora.virtuarealcraft.tool.Livers;
+import top.yora.virtuarealcraft.tool.TooltipTool;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class RainButterflyDust extends Item {
     public RainButterflyDust() {
         super(new Properties());
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
+        tooltip.add(Component.translatable("des.virtuarealcraft.rain_butterfly_dust").withStyle(ChatFormatting.GRAY));
+
+        TooltipTool.addLiverInfo(tooltip, Livers.AMEKI);
     }
 }
--- src/main/java/top/yora/virtuarealcraft/item/virtuareal19th/michiya/CursedArtifact.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal19th/michiya/CursedArtifact.java
@@ -1,9 +1,26 @@
 package top.yora.virtuarealcraft.item.virtuareal19th.michiya;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import top.yora.virtuarealcraft.tool.Livers;
+import top.yora.virtuarealcraft.tool.TooltipTool;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class CursedArtifact extends Item {
     public CursedArtifact() {
         super(new Properties());
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> tooltip, TooltipFlag pIsAdvanced) {
+        tooltip.add(Component.translatable("des.virtuarealcraft.cursed_artifact").withStyle(ChatFormatting.GRAY));
+
+        TooltipTool.addLiverInfo(tooltip, Livers.MICHIYA);
     }
 }
