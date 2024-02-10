6c99eed237d43332fb0a9d53b75562fff0deddf0
17146 <1714673995@qq.com>
2024-02-11 00:19:11 +0800

    做完了雨蝶之尘

--- src/main/java/top/yora/virtuarealcraft/item/virtuareal19th/ameki/RainButterflyDust.java
+++ src/main/java/top/yora/virtuarealcraft/item/virtuareal19th/ameki/RainButterflyDust.java
@@ -2,6 +2,9 @@ package top.yora.virtuarealcraft.item.virtuareal19th.ameki;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -13,8 +16,13 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public class RainButterflyDust extends Item {
+    private static final FoodProperties food = new FoodProperties.Builder().alwaysEat().nutrition(1).saturationMod(0.5f)
+            .effect(() -> new MobEffectInstance(MobEffects.GLOWING, 200), 1.0f)
+            .effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 200), 1.0f)
+            .effect(() -> new MobEffectInstance(MobEffects.LEVITATION, 200), 1.0f).fast().build();
+
     public RainButterflyDust() {
-        super(new Properties());
+        super(new Properties().food(food));
     }
 
     @Override
